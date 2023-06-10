a2f802eb99a3b760938ec28cde138753762a6938
KenRandolfMapa <2011880166@psu.palawan.edu.ph>
2023-06-10 16:38:50 +0800

    final drill

--- ken.py
+++ ken.py
@@ -1,7 +1,14 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
 
 app.config ["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -34,6 +41,13 @@ def get_customer_by_number(customerNumber):
     cur.execute(query)
     data = cur.fetchall()
     cur.close()
-	 
+
+@app.route("/employees", methods=["POST"])
+def add_employees():
+	cur = mysql.connection.cursor()
+	info = request.get_json()
+	firstName = info["firstName"]
+	lastName = info["lastName"]
+	
 if __name__ == "__main__":
 	app.run(debug=True)
